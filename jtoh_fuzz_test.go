2326096ed417165168bb94cfcd0e235d5e9e3489
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-03-27 13:08:40 +0200

    test: add combination of select + input on fuzz

--- jtoh_fuzz_test.go
+++ jtoh_fuzz_test.go
@@ -10,34 +10,78 @@ import (
 )
 
 func FuzzJTOH(f *testing.F) {
-	seedCorpus := [][]byte{
-		[]byte("string"),
-		[]byte(" "),
-		[]byte("{}"),
-		[]byte("[]"),
-		[]byte(`{ "name": "value"}`),
-		[]byte(`{ "name": 666}`),
-		[]byte(`{ "name": true}`),
-		[]byte(`[{ "name": "value"}]`),
-		[]byte(`[{ "name": 666}]`),
-		[]byte(`[{ "name": true}]`),
-		[]byte(`[{ "name": "value"}, {"name":666}]`),
-		[]byte(`{ "name": "value"}
+	type seed struct {
+		selector string
+		input    []byte
+	}
+	seedCorpus := []seed{
+		{
+			selector: ":a",
+			input:    []byte("string"),
+		},
+		{
+			selector: "/s",
+			input:    []byte(" "),
+		},
+		{
+			selector: "aa",
+			input:    []byte("{}"),
+		},
+		{
+			selector: ".field4",
+			input:    []byte("[]"),
+		},
+		{
+			selector: "%1",
+			input:    []byte(`{ "name": "value"}`),
+		},
+		{
+			selector: "<field",
+			input:    []byte(`{ "name": 666}`),
+		},
+		{
+			selector: "?xS",
+			input:    []byte(`{ "name": true}`),
+		},
+		{
+			selector: "|da",
+			input:    []byte(`[{ "name": "value"}]`),
+		},
+		{
+			selector: "^field^field2",
+			input:    []byte(`[{ "name": 666}]`),
+		},
+		{
+			selector: "#a#name",
+			input:    []byte(`[{ "name": true}]`),
+		},
+		{
+			selector: ":a:B:c",
+			input:    []byte(`[{ "name": "value"}, {"name":666}]`),
+		},
+		{
+			selector: ":name",
+			input: []byte(`{ "name": "value"}
 			{"name":666}`),
-		[]byte("\nmsg\nmsg2\nmsg2\n"),
+		},
+		{
+			selector: "@a@b",
+			input:    []byte("\nmsg\nmsg2\nmsg2\n"),
+		},
 	}
 
 	for _, seed := range seedCorpus {
-		f.Add(seed)
+		f.Add(seed.selector, seed.input)
 	}
 
-	f.Fuzz(func(t *testing.T, orig []byte) {
+	f.Fuzz(func(t *testing.T, selector string, orig []byte) {
 		input := bytes.NewReader(orig)
 		output := &bytes.Buffer{}
 
-		j, err := jtoh.New(":selector")
+		j, err := jtoh.New(selector)
 		if err != nil {
-			t.Fatal(f)
+			// It is expected that invalid selector will be generated
+			return
 		}
 
 		j.Do(input, output)
